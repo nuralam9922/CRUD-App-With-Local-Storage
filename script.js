36e3ea360d2156e3564f0dd9d108c115a28f519d
NUSH-9922 <nush9922@gmail.com>
2023-11-17 21:54:54 +0530

    js

--- script.js
+++ script.js
@@ -11,8 +11,6 @@ let addDataBtn = document.getElementById("btn")
 let data = [];
 
 // **************** run function when yuser click addDataBtn *************
-// ********************************** this function adds a new record *************************
-
 function add() {
     addDataBtn.onclick = function () {
         let newData = {
@@ -33,65 +31,72 @@ function add() {
             clearInputValues();
             alert("Data added successfully!");
         }
-    };
-}
-
-
-
-
-// ********************************** this function deletes the clicked item *************************
-function deleteRecord(dataIndex) {
-    data.splice(dataIndex, 1);
-    localStorage.setItem("userData", JSON.stringify(data));
-    setTableData();
-}
-
-// *********************************** this function edit ***************************************
-function edit(dataIndex) {
-    name.value = data[dataIndex].name;
-    age.value = data[dataIndex].age;
-    address.value = data[dataIndex].address;
-    email.value = data[dataIndex].email;
-
-    addDataBtn.onclick = function () {
-        data[dataIndex].name = name.value;
-        data[dataIndex].age = age.value;
-        data[dataIndex].address = address.value;
-        data[dataIndex].email = email.value;
-        localStorage.setItem("userData", JSON.stringify(data));
-        setTableData();
-        clearInputValues();
     }
 }
 
-// *********************************** this function sets up the table ***************************************
+// let allDeleteBtns = document.querySelectorAll(".Delete");
 function setTableData() {
     table.innerHTML = `
-    <tr>
-        <th>Name</th>
-        <th>Age</th>
-        <th>Address</th>
-        <th>Email</th>
-        <th>Actions</th>
-    </tr>`;
+        <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Address</th>
+            <th>Email</th>
+            <th>Actions</th>
+        </tr>`;
 
     for (let i = 0; i < data.length; i++) {
         table.innerHTML += `<tr>
-            <td>${data[i].name}</td>
-            <td>${data[i].age}</td>
-            <td>${data[i].address}</td>
-            <td>${data[i].email}</td>
-            <td class="deleteAndEdit">
-                <p class="Delete" onclick="deleteRecord(${i})">Delete</p>
-                <p class="Edit" onclick="edit(${i})">Edit</p>
-            </td>
-        </tr>`;
+                <td>${data[i].name}</td>
+                <td>${data[i].age}</td>
+                <td>${data[i].address}</td>
+                <td>${data[i].email}</td>
+                <td class="deleteAndEdit"><p class="Delete">Delete</p><p class="Edit">Edit</p></td>
+            </tr>`;
     }
 
-  
+    edit();
+    delate();
+}
+// ********************************** this function delete the clicked item *************************
+function delate() {
+    let allDeleteBtns = document.querySelectorAll(".Delete");
+    allDeleteBtns.forEach((deleteBtn, dataIndex) => {
+        deleteBtn.onclick = function () {
+            data.splice(dataIndex, 1);
+            localStorage.setItem("userData", JSON.stringify(data));
+            setTableData();
+        };
+    });
 }
 
+// *********************************** edit value ***************************************888
 
+function edit() {
+    let allEditBtns = document.querySelectorAll(".Edit");
+    allEditBtns.forEach((editBtn, dataIndex) => {
+        editBtn.onclick = function () {
+            name.value = data[dataIndex].name;
+            age.value = data[dataIndex].age;
+            address.value = data[dataIndex].address;
+            email.value = data[dataIndex].email;
+            console.log(data[dataIndex].age);
+            addDataBtn.onclick = function () {
+                data[dataIndex].name = name.value;
+                data[dataIndex].age = age.value;
+                data[dataIndex].address = address.value;
+                data[dataIndex].email = email.value;
+                localStorage.setItem("userData", JSON.stringify(data));
+                setTableData();
+                clearInputValues();
+                addDataBtn.innerHTML = 'Add Data';
+                add();
+            }
+            addDataBtn.innerHTML = 'Edit Data';
+        };
+    });
+
+}
 
 
 
@@ -99,10 +104,10 @@ function setTableData() {
 
 // ************************** Clear all input fild values ***************************
 function clearInputValues() {
-    age.value = ""
-    address.value = ""
-    email.value = ""
-    name.value = ""
+    age.value = "";
+    address.value = "";
+    email.value = "";
+    name.value = "";
 
 }
 
